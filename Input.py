eac431f9d36a0f1821a4ea9556e98becfb2a0c3a
syauqisnh <107410391+syauqisnh@users.noreply.github.com>
2022-06-13 19:34:56 +0700

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,13 @@
+#Cara Mengambil Data dari user
+
+Nama = input("Masukkan Nama Anda : ")
+print("Nama :", Nama, "- Type Data : ", type(Nama))
+
+Nim = int(input("Masukkan Nim Anda : "))
+print("Nim :", Nim, "- Type Data : ", type(Nim))
+
+Nilai = float(input("Masukkan Nilai Anda : "))
+print("Nilai :", Nilai, "- Type Data : ", type(Nilai))
+
+biner = bool(int(input("Masukkan Boolean = ")))
+print("data :", biner, "- Type Data : ", type(biner))
