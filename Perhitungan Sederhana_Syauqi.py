8a1829ded0377b597a205cb7b4a26c219ad2c777
Syauqi Nur Hibatullah <107410391+syauqisnh@users.noreply.github.com>
2022-06-15 17:35:56 +0700

    Add files via upload

---
+++ Perhitungan Sederhana_Syauqi.py
@@ -0,0 +1,27 @@
+print("\nPR\n")
+print("Nama : Syauqi Nur Hibatullah")
+print("Asal Instansi : Universitas Sebelas Maret")
+print("Prodi : D3 Teknik Informatika")
+print("\nSOAL : \n")
+print("1. Buatlah Perhitungan Luas Segi Tiga ? ")
+print("2. Buatlah Perhitungan Luas Persegi Panjang ?")
+print("\nJawaban : \n")
+
+print("===NOMOR 1===")
+
+alas = float(input("Masukkan Alas Segitiga = "))
+tinggi = float(input("Masukkan Tinggi Segitiga = "))
+
+Luas = (alas * tinggi)/2
+
+print("1. Luas Segitiga adalah = %0.2f" %Luas)
+
+print("===NOMOR 2===")
+
+Panjang = float(input("Masukkan Panjang = "))
+Lebar = float(input("Masukkan Lebar = "))
+
+Luas = (Panjang * Lebar)
+print("2. Luas Persegi Panjang Adalah = %0.2f" %Luas)
+
+print("SELESAI")
