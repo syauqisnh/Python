df37c955756368724f9dfaf9dc74b3ca63089bf6
Syauqi Nur Hibatullah <107410391+syauqisnh@users.noreply.github.com>
2022-06-15 17:37:05 +0700

    Add files via upload

---
+++ Fisika Menggunakan python.py
@@ -0,0 +1,16 @@
+print("\nLATIHAN 1\n")
+print("Soal : ")
+print("Buatlah Sebuah Program Konfrensi Temperatur ?")
+print("\nJawabannya :\n")
+
+celcius = float(input("Masukkan suhu dalam celcius = "))
+print("Suhunya adalah = ", celcius, "Celcius")
+
+reamur = (4/5)*celcius #Rumus nya Bro
+print("Suhunya dalam reamur adalah = ", reamur, "Reamur")
+
+Fahrenheit = ((9/5)*celcius)+32 #Rumus nya Bro
+print("Suhunya dalam Fahrenheit adalah = ", Fahrenheit, "Fahrenheit")
+
+kelvin = celcius + 273 #Rumus nya Bro
+print("Suhunya dalam kelvin adalah = ", kelvin, "Kelvin")
